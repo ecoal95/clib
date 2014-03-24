208b51f0c729e53e3213004ec6fb45d325369298
Emilio Cobos Álvarez <ecoal95@gmail.com>
2014-03-24 10:55:09 +0100

    Fix expression warnings

--- src/core/utils.h
+++ src/core/utils.h
@@ -25,22 +25,22 @@
 }
 #else
 #define return_if(expr) if(expr) { \
-	fprintf(stderr, "[WARNING] [%s:%d:%s] #expr passed\n", __FILE__, __LINE__, __FUNCTION__); \
+	fprintf(stderr, "[WARNING] [%s:%d:%s] "#expr" passed\n", __FILE__, __LINE__, __FUNCTION__); \
 	return;                       \
 }
 
 #define return_val_if(expr, val) if(expr) { \
-	fprintf(stderr, "[WARNING] [%s:%d:%s] #expr passed\n", __FILE__, __LINE__, __FUNCTION__); \
+	fprintf(stderr, "[WARNING] [%s:%d:%s] "#expr" passed\n", __FILE__, __LINE__, __FUNCTION__); \
 	return val;                            \
 }
 
 #define return_if_fail(expr) if(!(expr)) { \
-	fprintf(stderr, "[WARNING] [%s:%d:%s] #expr failed\n", __FILE__, __LINE__, __FUNCTION__); \
+	fprintf(stderr, "[WARNING] [%s:%d:%s] "#expr" failed\n", __FILE__, __LINE__, __FUNCTION__); \
 	return;                               \
 }
 
 #define return_val_if_fail(expr, val) if(!(expr)) { \
-	fprintf(stderr, "[WARNING] [%s:%d:%s] #expr failed\n", __FILE__, __LINE__, __FUNCTION__); \
+	fprintf(stderr, "[WARNING] [%s:%d:%s] "#expr" failed\n", __FILE__, __LINE__, __FUNCTION__); \
 	return val;                                 \
 }
 #endif

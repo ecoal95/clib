d6e8ca18f38b07bc2fa97cd72b6d9a1f74197534
Emilio Cobos Álvarez <ecoal95@gmail.com>
2014-10-02 10:55:58 +0200

    Update date.h

--- src/common/date.h
+++ src/common/date.h
@@ -1,19 +1,61 @@
 #ifndef __DATE_H
 #define __DATE_H
-#include "str.h"
+
+#include <time.h>
+
+typedef struct tm Date;
+
 /**
- * La función de los romanos es casi más de la biblioteca de las fechas pero bueno...
- * En vez de hacer un void retornamos la cadena
+ * Get a Date object with current time
+ *
+ * @return Date *
  */
-char * date(const char * format, const time_t time) {
-	struct tm * t;
-	if( strcmp("c", format) == 0 ) {
-		return ctime(time);
-	}
+Date * newDate() {
+	time_t time = time(NULL);
 
-	t = localtime(&time);
+	return_null_if(time == (time_t) -1);
 
-
+	return localtime(&time);
 }
 
+/**
+ * Get a date from a string
+ *
+ * @param const char * str
+ *
+ * @return Date *
+ */
+Date * Date__parse(const char * str) {
+	return getdate(str);
+}
+
+/**
+ * Get a date's time
+ *
+ * @param Date * self
+ *
+ * @return time_t
+ */
+time_t Date_getTime(Date * self) {
+	return mktime(self);
+}
+
+/**
+ * Set a date's time
+ *
+ * @param Date * self
+ * @param time_t time
+ *
+ * @return Date *
+ */
+Date * Date_setTime(Date * self, time_t time) {
+	self->tm_sec = time;
+	self->tm_mon = 0;
+	self->tm_mday = 0;
+	self->tm_year = 1970;
+
+	mktime(self);
+
+	return self;
+}
 #endif

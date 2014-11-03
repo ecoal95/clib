92dab56c2784dd7d14e6846d30e4d2f1b34e3299
Emilio Cobos Álvarez <ecoal95@gmail.com>
2014-11-03 17:34:40 +0100

    Create SortingUtils.h

---
+++ src/common/SortingUtils.h
@@ -0,0 +1,43 @@
+#ifndef __SORTING_UTILS_H
+#define __SORTING_UTILS_H
+/** A sorting comparison function must receive the two values, and a custom data pointer */
+typedef int (*SortingComparison)(const void *, const void *, void *);
+
+/** A sorting function must receive the vector, the size, the comparison function, and a custom data pointer */
+typedef void ** (*SortingFunction)(void **, size_t, SortingComparison, void *);
+
+/**
+ * Sorting a vector of pointers using bubble sort algorithm
+ *
+ * @param void **vector
+ * @param size_t length
+ * @param SortingComparison cmp
+ * @param void *data
+ *
+ * @return void ** the same vector
+ */
+void ** SortingUtils__bubbleSort(void **vector, size_t length, SortingComparison cmp, void *data);
+
+/**
+ * Sorting comparisong for descending order of ints
+ *
+ * @param void *v1
+ * @param void *v2
+ * @param void *data
+ *
+ * @return int
+ */
+int SortingUtils__INT_DESCENDING(const void *v1, const void *v2, void *data);
+
+/**
+ * Sorting comparisong for ascending order of ints
+ *
+ * @param void *v1
+ * @param void *v2
+ * @param void *data
+ *
+ * @return int
+ */
+int SortingUtils__INT_ASCENDING(const void *v1, const void *v2, void *data);
+
+#endif

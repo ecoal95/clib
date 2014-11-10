975d4b56788d6596a4d2e279cea62dcaff43c0bd
Emilio Cobos Álvarez <ecoal95@gmail.com>
2014-11-10 17:36:41 +0100

    Update SortingUtils.c

--- src/common/SortingUtils.c
+++ src/common/SortingUtils.c
@@ -1,4 +1,10 @@
 #include "SortingUtils.h"
+
+// Just for code comprehension
+// This is a functional macro so it's undef'd at the end of this file
+// Last semicolon has been removed intentionally
+#define SU_SWAP(a, b) temp = a; a = b; b = temp
+
 /**
  * Sorting a vector of pointers using bubble sort algorithm
  *
@@ -19,9 +25,7 @@ void ** SortingUtils__bubbleSort(void **vector, size_t length, SortingComparison
 	for ( ; i < length; i++ ) {
 		for ( j = 0; j < length - i; i++ ) {
 			if ( cmp(vector[j], vector[j + 1], data) > 0 ) {
-				temp = vector[j];
-				vector[j] = vector[j + 1];
-				vector[j + 1] = temp;
+				SU_SWAP(vector[j], vector[j + 1]);
 			}
 		}
 	}
@@ -51,11 +55,93 @@ void ** SortingUtils__selectSort(void **vector, size_t length, SortingComparison
 				lower_index = j;
 			}
 		}
-		temp = vector[i - 1];
-		vector[i - 1] = vector[lower_index];
-		vector[lower_index] = temp;
+		SU_SWAP(vector[i - 1], vector[lower_index]);
 	}
 
+	return vector;
+}
+
+/**
+ * Sorting a vector of pointers using insert sort algorithm
+ *
+ * @param void **vector
+ * @param size_t length
+ * @param SortingComparison cmp
+ * @param void *data
+ *
+ * @return void ** the same vector
+ */
+void ** SortingUtils__insertSort(void **vector, size_t length, SortingComparison cmp, void *data) {
+	void *temp;
+	void *unsorted;
+	size_t i;
+	size_t j;
+
+	// Assume the first element is sorted, we'll swap later
+	for ( i = 1; i < length; i++ ) {
+		unsorted = vector[i];
+		j = i;
+		// We go to the left
+		while ( j-- ) {
+			// If the current element is smaller than the left one
+			// Move left element one position to the rigth
+			if ( cmp(unsorted, vector[j], data) > 0 ) {
+				SU_SWAP(vector[j], vector[j + 1]);
+			} else {
+				// Else we stop, we have to insert element on j + 1
+				break;
+			}
+		}
+
+		// insert the element in the last position where it 
+		vector[j + 1] = unsorted;
+	}
+
+	return vector;
+}
+
+/**
+ * Sorting a vector of pointers using quick sort algorithm
+ *
+ * @param void **vector
+ * @param size_t length
+ * @param SortingComparison cmp
+ * @param void *data
+ *
+ * @return void ** the same vector
+ */
+void ** SortingUtils__quickSort(void **vector, size_t length, SortingComparison cmp, void *data) {
+	void *temp;
+	size_t pivot_index = 0;
+	size_t store_index = 1; // store_index is the index of the first element which is greater than the pivot
+	size_t i;
+
+	if ( length == 1 ) {
+		return;
+	}
+
+	for ( i = 1; i < length; i++) {
+		if ( cmp(vector[i], vector[pivot_index], data) > 0 ) {
+			// if element is smaller than the pivot
+			// swap it with the first element greater than it
+			// So the first element greater than it is potencially the next one
+			if ( store_index != i ) { // Just avoid unnecessary swaps
+				SU_SWAP(vector[store_index], vector[i]);
+			}
+			store_index++;
+		}
+	}
+
+	// Move the pivot to a location where all the left elements are smaller than it
+	// and all the right elements are greater than it
+	SU_SWAP(vector[pivot_index], vector[store_index - 1]);
+
+	// Sort the smaller elements
+	SortingUtils__quickSort(vector, store_index, cmp, data);
+
+	// And sort the greater ones
+	SortingUtils__quickSort(vector + store_index, length - store_index, cmp, data);
+	
 	return vector;
 }
 
@@ -87,3 +173,5 @@ int SortingUtils__INT_DESCENDING(const void *v1, const void *v2, void *data) {
 int SortingUtils__INT_ASCENDING(const void *v1, const void *v2, void *data) {
 	return -SortingUtils__INT_DESCENDING(v1, v2, data);
 }
+
+#undef SU_SWAP
